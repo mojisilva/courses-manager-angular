d6b6f9ebf9473cdb41aad744e75c053e2afd76a1
mojisilva <mojiasilva@gmail.com>
2022-05-16 15:16:06 -0300

    creation of routers for files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
+import { CourseInfoComponent } from './courses/courses-info.component';
+import { Error404Component } from './error-404/error-404.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/couse-list.component';
+import { RouterModule } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -14,10 +18,29 @@ import { AppComponent } from './app.component';
     CourseListComponent,
     StarComponent,
     ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component
+      },
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses/courses-info.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  templateUrl: './courses-info.component.html',
+})
+export class CourseInfoComponent implements OnInit{
+
+    courseId: number;
+
+    constructor(private activatedRoute: ActivatedRoute){
+
+    }
+
+    ngOnInit(): void {
+      this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+    }
+}
